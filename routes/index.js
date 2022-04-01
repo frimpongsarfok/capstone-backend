3b036050e214837ca6286ace82edd9c4ff4f7b75
Sarfo Kantanka Frimpong <frimpongsarfok@gmail.com>
2022-04-01 12:49:17 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,119 @@ const saltRounds=10;
 const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV || 'development']);
 //INVITE USER
 const clientAddress='https://capstone-frontend-sdi08.herokuapp.com'
-router.get('/',(req,res)=>res.send('<h1  >Code-Talker API</h1>'))
+router.get('/',(req,res)=>res.send(`
+<center>
+<h1>Code-Talker API</h1>
+<div>
+<h2>API CALLS</h2>
+<h4>Login</h4>
+<h5>GET:<span>https://capstone-sdio8-codetalkers.herokuapp.com/login?username=whywhy&password=why</span></h5>
+
+<h4>Signup</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/signup?username=brandon&password=1234</span></h5>
+
+<h4>Logout</h4>
+<h5>Get :<span>https://capstone-sdio8-codetalkers.herokuapp.com/logout</span></h5>
+
+<h4>Users</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/users?user=s'</span></h5>
+
+<h4>Products</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products</span></h5>
+
+<h4>Products</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products</span></h5>
+
+<h4>Products ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products?id=1</span></h5>
+
+<h4>Products User ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/users/products?users_id=1</span></h5>
+
+<h4>Card</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?name=Live Fire&tags=Range 40&points_value=1&due_date=2022-03-30&completed=0&list_id=1</span></h5>
+
+<h4>Card List ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?list_id=1</span></h5>
+
+<h4>List</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list</span></h5>
+
+<h4> List Products ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list?product_id=1</span></h5>
+
+<h4>Products ID</h4>
+<h5>GET :<span></span></h5>
+
+<h4>Update Account</h4>
+<h5>PUT :<span></span>https://capstone-sdio8-codetalkers.herokuapp.com/account?username=s&password=1234&fname=sarfo&lname=f</h5>
+
+<h4>Delete Account</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/deleteAccount</span></h5>
+
+<h4>Projects</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/projects</span></h5>
+
+<h4>ADD Projects</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/projects?name=capstone</span></h5>
+
+<h4>Delete Project</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/projects?projects_id=1</span></h5>
+
+<h4>Update Project name</h4>
+<h5>PATCH :<span>https://capstone-sdio8-codetalkers.herokuapp.com/projects?new_project_name=democalc&project_id=1</span></h5>
+
+<h4>Project ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products?project_id=2</span></h5>
+
+<h4>Products name within a project</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products?project_id=2&name=product_name</span></h5>
+
+<h4>Update Products</h4>
+<h5>PATCH :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products?new_product_name=christmas&product_id=1</span></h5>
+
+<h4>Delete Products ID</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/products?product_id=1</span></h5>
+
+<h4>Add list</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list?product_id=1&name=hello</span></h5>
+
+<h4>Products ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list?product_id=1</span></h5>
+
+<h4>Update list</h4>
+<h5>PATCH :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list?new_list_name=ToDo&list_id=1</span></h5>
+
+<h4>Delete list</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/list?list_id=8</span></h5>
+
+<h4>Card List ID</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?list_id=1&name=running</span></h5>
+
+<h4>Add card to List</h4>
+<h5>POST :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?list_id=1&name=running</span></h5>
+
+<h4>Card</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?list_id=2</span></h5>
+
+<h4>Card</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?list_id=2</span></h5>
+
+<h4>Add Card</h4>
+<h5>PATCH :<span>https://capstone-sdio8-codetalkers.herokuapp.com/card?card_id=1&new_card_name=JRTC</span></h5>
+
+<h4>Invite user</h4>
+<h5>GET :<span>https://capstone-sdio8-codetalkers.herokuapp.com/invite?new_user_id=2&project_id=1&new_user_permission=3</span></h5>
+
+<h4>Remove User</h4>
+<h5>DEL :<span>https://capstone-sdio8-codetalkers.herokuapp.com/remove_user_from_project?user_id=2&project_id=1</span></h5>
+
+<h4>Update User Permission</h4>
+<h5>PATCH :<span>https://capstone-sdio8-codetalkers.herokuapp.com/update_user_permission?user_id=2&project_id=1&new_permission=2</span></h5>
+</div>
+</center>
+
+`))
 router.post('/invite',(req,res)=>{
   res.header('Access-Control-Allow-Origin',clientAddress)
   .header('Access-Control-Allow-Credentials', true)
